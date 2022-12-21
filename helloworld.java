4fd6ef8cb90e284ca8ef6a37e78edf752f1c38a6
varshashingote <120667268+varshashingote@users.noreply.github.com>
2022-12-21 11:24:06 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+public class helloworld
+  {
+     public static void main( String args[])
+       {
+        while(true)
+            {
+                System.out.println("Hello WOrld");
+                 try
+                  {
+ 
+                        Thread.sleep(60000);
+
+                  }
+                  catch(Exception ex){}
+
+             }
+        }
+  }
+
