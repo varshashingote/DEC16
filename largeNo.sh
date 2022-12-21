bd9df1cf272360c43063517d069d8dbb46eccf86
varshashingote <120667268+varshashingote@users.noreply.github.com>
2022-12-21 18:50:26 +0530

    Add files via upload

---
+++ largeNo.sh
@@ -0,0 +1,21 @@
+read -p "enter the Numbers: " a
+read -p "Enter the Number: " b
+        if [ $a  -gt  $b ]
+        then
+
+                echo " $a is greater than"
+        else
+                echo " $b is largest number "
+        fi
+
+
+
+
+
+
+
+
+
+
+
+
